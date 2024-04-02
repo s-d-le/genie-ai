bdfb98a880f53ef92f793f5c3c2433fda0c57f66
Son Le <ayzkeys@gmail.com>
2024-04-02 11:24:01 +0700

    navbar placeholder

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <>
       <p className="text-6xl text-green-500">Hello AI World!</p>
-      <Button size="lg">Go</Button>
+      <UserButton afterSignOutUrl="/" />
     </>
   );
 }
