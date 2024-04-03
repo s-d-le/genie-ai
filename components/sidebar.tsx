3e9cc10569014a63a60844b24be1045ca9738adf
Son Le <ayzkeys@gmail.com>
2024-04-03 15:21:39 +0700

    more navbar items

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -13,6 +13,7 @@ import {
   VideoIcon,
   CodeIcon,
 } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
 
@@ -61,6 +62,9 @@ const routes = [
 ];
 
 const Sidebar = () => {
+  // To get current pathname
+  const pathname = usePathname();
+
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
@@ -77,7 +81,12 @@ const Sidebar = () => {
             <Link
               key={route.href}
               href={route.href}
-              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+              className={cn(
+                pathname === route.href
+                  ? "text-white bg-white/10"
+                  : "text-zinc-400",
+                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+              )}
             >
               <div className="flex items-center flex-1">
                 <route.icon className={cn(route.color, "h-5 w-5 mr-3")} />
